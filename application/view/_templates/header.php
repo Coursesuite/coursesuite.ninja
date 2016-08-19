fed82da6b6532981aa68d0cc5fe29c3b7f2fd6d3
Tim St.Clair <tim.stclair@gmail.com>
2016-08-19 16:40:22 +1000

    fixed analytics url

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -45,7 +45,7 @@ if (isset($google_analytics_id) && (!empty($google_analytics_id))) {
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
       ga('create', '" . $google_analytics_id . "', 'auto');
       ga('send', 'pageview');
@@ -105,7 +105,7 @@ if (isset($google_analytics_id) && (!empty($google_analytics_id))) {
             echo "<a href='{$baseurl}register/index/freeTrial' class='free-trial green-button'>" . KeyStore::find("freetriallabel")->get() . "</a>";
         } elseif (UserModel::getTrialAvailability(intval(Session::get('user_id'))) && KeyStore::find("freetrial")->get()=="true" && !empty(KeyStore::find("freetriallabel")->get())) {
             echo "<a href='{$baseurl}register/registeredUserTrial' class='free-trial green-button'>" . KeyStore::find("freetriallabel")->get() . "</a>";
-        } 
+        }
 
         ?>
     </header>
