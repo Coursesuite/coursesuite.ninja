df7830378e9c1bf19a8447cfd60be50687b3c38c
Tim St.Clair <tim.stclair@gmail.com>
2016-09-02 12:38:18 +1000

    A css solution to the footer not sticking to the bottom in IE. The footer is still available using this solution. See https://philipwalton.com/articles/normalizing-cross-browser-flexbox-bugs/

--- application/view/_templates/footer.php
+++ application/view/_templates/footer.php
@@ -1,13 +1,5 @@
     </main>
 
-    <style>
-    @media all and (-ms-high-contrast:none)
-    {
-    	.footer {display:none;} /* IE10 */
-    	*::-ms-backdrop, .footer {display:none;} /* IE11 */
-    }
-    </style>
-
     <footer class='footer'>
 	    <div class="standard-width">
 		    <div class="footer-columns">
@@ -17,7 +9,7 @@
 		    </div>
 	    </div>
     </footer>
-	
+
 
     <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
     <script>window.jQuery || document.write('<script src="<?=Config::get('URL')?>js/jquery-1.10.2.min.js"><\/script>')</script>
